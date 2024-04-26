728dbe8a2bae46c8a27d41c65409cbb836c9d656
Exequiel <aaronleal03@gmail.com>
2024-04-25 23:57:27 -0400

    Agregado JS

---
+++ js/contacto.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#enviarb").submit(function(e){
+
+        e.preventDefault();
+
+        let Motivo = $("#motivo").val().trim()
+        let NombreApellido = $("#nombreap").val().trim();
+        let CorreoE = $("#correo").val().trim();
+        let Telefono = $("#telefono").val().trim();
+        let Mensaje = $("#mensaje").val().trim();
+
+
+
+
+    })
+
+})
