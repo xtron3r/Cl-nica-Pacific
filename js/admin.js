dab75d0240d2b0fb06546fde97f2d572f6786e4e
Exequiel <aaronleal03@gmail.com>
2024-05-04 22:36:28 -0400

    Error Admin.js, Arreglo css :hover

--- js/admin.js
+++ js/admin.js
@@ -3,29 +3,49 @@ $(document).ready(function() {
     $('#contacto').hide();
     $('#pacientes').hide();
     $('#medicos').hide();
+    $('#AgregMedico').hide();
+    
 
     $('#btn-contacto').click(function() {
         $('#contacto').show();
         $('#pacientes').hide();
         $('#medicos').hide();
+        $('#AgregMedico').hide();
     });
 
     $('#btn-medico').click(function() {
         $('#medicos').show();
         $('#contacto').hide();
         $('#pacientes').hide();
+        $('#AgregMedico').hide();
     });
 
     $('#btn-paciente').click(function() {
         $('#pacientes').show();
         $('#contacto').hide();
         $('#medicos').hide();
+        $('#AgregMedico').hide();
     });
 
     $('#btn-limpiar').click(function() {
         $('#pacientes').hide();
         $('#contacto').hide();
         $('#medicos').hide();
+        $('#AgregMedico').hide();
+    });
+
+    $('#btn-agregarM').click(function() {
+        $('#AgregMedico').show();
+        $('#contacto').hide();
+        $('#pacientes').hide();
+        $('#medicos').hide();
+    });
+
+    $('#btn-cancelarM').click(function() {
+        $('#medicos').show();
+        $('#pacientes').hide();
+        $('#contacto').hide();
+        $('#AgregMedico').hide();
     });
 
 });
