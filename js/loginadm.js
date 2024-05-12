6724f644518c641dfa484be5465eff73dcca8171
Exequiel <aaronleal03@gmail.com>
2024-05-12 15:18:40 -0400

    Se coloco JQuery validate para el login de admin

--- js/loginadm.js
+++ js/loginadm.js
@@ -3,17 +3,36 @@ $(document).ready(function(){
         rules:{
            FormRut:{
                 required: true,
-                minlength: 5
+                number: true,
+                minlength: 9,
+                maxlength:9
+           },
+           FormContra:{
+                required:true,
+                minlength:5
            }
+
         },
         messages: {
             FormRut: {
-                required: "Por Favor ingrese ",
+                required: "Por Favor ingrese rut ",
+                number: "Por favor ingrese solo numeros",
+                minlength: "El minimo de caracteres es 9",
+                maxlength: "El maximo de caracteres es 9"
+            },
+            FormContra: {
+                required: "Por Favor ingrese contraseña",
                 minlength: "El minimo de caracteres es 5"
             }
             
-            
         },
-        submitHandler: function(form) {}
+        submitHandler: function(form) {
+            redir()
+        }
     });
 });
+
+
+function redir(){
+    window.location.href = "admin.html";
+}
