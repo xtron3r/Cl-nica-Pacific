ae9ff3328f6f7aa07119951db14b8bb25a26ba34
Estebandido123 <este.toledo@duocuc.cl>
2024-05-16 19:58:31 -0400

    actualizacion

--- js/anular.js
+++ js/anular.js
@@ -1,107 +1,118 @@
 document.addEventListener('DOMContentLoaded', function() {
-  function validarRut(rut) {
-      // Eliminar puntos y guion del rut
-      rut = rut.replace(/\./g, '').replace(/\-/g, '');
-
-      // Separar el dígito verificador
-      var dv = rut.slice(-1);
-      var rutSinDV = rut.slice(0, -1);
-
-      // Validar rut
-      if (/^\d+$/.test(rutSinDV) === false) return false;
-
-      // Calcular dígito verificador esperado
-      var suma = 0;
-      var multiplo = 2;
-
-      for (var i = rutSinDV.length - 1; i >= 0; i--) {
-          suma += parseInt(rutSinDV.charAt(i)) * multiplo;
-          multiplo = multiplo === 7 ? 2 : multiplo + 1;
-      }
-
-      var dvEsperado = 11 - (suma % 11);
-
-      // Si el dígito verificador es 11, cambiar a 0
-      dvEsperado = (dvEsperado === 11) ? 0 : ((dvEsperado === 10) ? 'K' : dvEsperado);
-
-      // Validar dv
-      if (dvEsperado != dv.toUpperCase()) return false;
-
-      return true;
-  }
-
-  function validarFormulario() {
-      var rutInput = document.getElementById('txt_rut');
-      var rut = rutInput.value.trim();
-      var mensajeError = document.getElementById('mensaje-error');
-
-      mensajeError.innerHTML = '';
-      if (rut === '') {
-          mensajeError.innerHTML = 'Debe ingresar un RUT.';
-          return false;
-      }
-      if (rut.length > 12) {
-          mensajeError.innerHTML = 'El RUT ingresado es demasiado largo.';
-          return false;
-      }
-      if (rut.length < 8) {
-          mensajeError.innerHTML = 'El RUT ingresado es demasiado corto.';
-          return false;
-      }
-
-      if (!validarRut(rut)) {
-          mensajeError.innerHTML = 'El RUT ingresado no es válido.';
-          return false;
-      }
-
-      return true;
-  }
-  document.getElementById('btnvalida').addEventListener('click', function() {
-      if (validarFormulario()) {
-          var rut = localStorage.getItem("rut");
-          var fecha = localStorage.getItem("fecha");
-          var hora = localStorage.getItem("hora");
-          var nombre = localStorage.getItem("nombre");
-
-          if (rut && fecha && hora && nombre) {
-              var rut = document.getElementById('txt_rut').value.trim();
-              var tableRows = document.querySelectorAll("#tableBody tr");
-              var rutExistente = false;
-
-              if (!rutExistente) {
-                  var newRow = document.createElement("tr"); //crear el tr
-                  newRow.innerHTML = `
-                      <td>${nombre}</td>
-                      <td>${fecha}</td>
-                      <td>${hora}</td>
-                      <td>
-                          <button type="button" class="btn btn-danger btn-anular fw-bold" onclick="eliminar(this)">
-                              <i class="bi bi-trash"></i>
-                          </button>
-                      </td>
-                  `;
-
-                  // Verificar si ya existe un tbody en la tabla
-                  var tableBody = document.getElementById("tableBody");
-                  if (!tableBody) {
-                      // Si no existe, crear uno nuevo
-                      tableBody = document.createElement("tbody");
-                      tableBody.id = "tableBody";
-                      document.querySelector("table").appendChild(tableBody);
-                  }
-
-                  // Agregar la fila al tbody
-                  tableBody.appendChild(newRow);
-              } else {
-                  alert("El Rut ya existe");
-              }
-          } else {
-              alert("No hay datos guardados");
-          }
-      }
+    function validarRut(rut) {
+        rut = rut.replace(/\./g, '').replace(/\-/g, '');
+  
+        var dv = rut.slice(-1).toUpperCase();
+        var rutSinDV = rut.slice(0, -1);
+  
+        if (!/^\d+$/.test(rutSinDV)) return false;
+  
+        var suma = 0;
+        var multiplo = 2;
+  
+        for (var i = rutSinDV.length - 1; i >= 0; i--) {
+            suma += parseInt(rutSinDV.charAt(i)) * multiplo;
+            multiplo = multiplo === 7 ? 2 : multiplo + 1;
+        }
+  
+        var dvEsperado = 11 - (suma % 11);
+        dvEsperado = (dvEsperado === 11) ? '0' : ((dvEsperado === 10) ? 'K' : dvEsperado.toString());
+  
+        return dvEsperado === dv;
+    }
+  
+    function mensajeError(mensaje) {
+        var mensajeError = document.getElementById('mensaje-error');
+        mensajeError.innerHTML = mensaje;
+    }
+  
+    function validarFormulario() {
+        var rutInput = document.getElementById('txt_rut');
+        var rut = rutInput.value.trim();
+  
+        mensajeError('');
+        if (rut === '') {
+            mensajeError('Debe ingresar un RUT.');
+            return false;
+        }
+        if (rut.length > 12) {
+            mensajeError('El RUT ingresado es demasiado largo.');
+            return false;
+        }
+        if (rut.length < 8) {
+            mensajeError('El RUT ingresado es demasiado corto.');
+            return false;
+        }
+  
+        if (!validarRut(rut)) {
+            mensajeError('El RUT ingresado no es válido.');
+            return false;
+        }
+  
+        return true;
+    }
+  
+    function agregarFila(nombre, fecha, hora) {
+        var newRow = document.createElement("tr");
+        newRow.innerHTML = `
+            <td>${nombre}</td>
+            <td>${fecha}</td>
+            <td>${hora}</td>
+            <td>
+                <button type="button" class="btn btn-danger btn-anular fw-bold" onclick="eliminar(this)">
+                    <i class="bi bi-trash"></i>
+                </button>
+            </td>
+        `;
+  
+        var tableBody = document.getElementById("tableBody");
+        if (!tableBody) {
+            tableBody = document.createElement("tbody");
+            tableBody.id = "tableBody";
+            document.querySelector("table").appendChild(tableBody);
+        }
+  
+        tableBody.appendChild(newRow);
+    }
+  
+    function buscarDatos() {
+        if (validarFormulario()) {
+            var rut = document.getElementById('txt_rut').value.trim();
+            var fecha = localStorage.getItem("fecha");
+            var hora = localStorage.getItem("hora");
+            var nombre = localStorage.getItem("nombre");
+  
+            if (localStorage.getItem("rut") === rut) {
+                var tableBody = document.getElementById("tableBody");
+                var rutExistente = Array.from(tableBody.rows).some(row => row.cells[0].textContent === nombre);
+  
+                if (!rutExistente) {
+                    agregarFila(nombre, fecha, hora);
+                } else {
+                    
+                }
+            } else {
+                mensajeError("El rut no existe");
+            }
+        }
+    }
+  
+    document.getElementById('btnvalida').addEventListener('click', function(event) {
+        event.preventDefault();
+        buscarDatos();
+    });
   });
-});
+  
+  function eliminar(button) {
+    var row = button.closest('tr');
+    var nombre = row.cells[0].textContent;
 
-function eliminar(button) {
-  button.closest('tr').remove();
+    // Eliminar del localStorage si coincide con el RUT
+    if (localStorage.getItem("nombre") === nombre) {
+        localStorage.removeItem("rut");
+        localStorage.removeItem("fecha");
+        localStorage.removeItem("hora");
+        localStorage.removeItem("nombre");
+    }
+    row.remove();
 }
